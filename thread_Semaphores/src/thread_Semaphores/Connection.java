d685ed4b5e8dbfded21e1b27cd149159fab2b55a
manibhushan <manibhushan.cs@gmail.com>
2016-07-19 07:31:16 +0530

    semaphores demo with connection threads limit

---
+++ thread_Semaphores/src/thread_Semaphores/Connection.java
@@ -0,0 +1,50 @@
+package thread_Semaphores;
+
+import java.util.concurrent.Semaphore;
+
+public class Connection {
+
+	private static Connection conn = new Connection();
+	private int connections = 0;
+	private Semaphore sem = new Semaphore(10);
+	
+	private Connection () {}
+	
+	public static Connection getInstance () {
+		return conn;
+	}
+	
+	public void connect() {
+	
+		try {
+			sem.acquire();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			doConnect();
+		} finally {
+			sem.release();
+		}
+	}
+	
+	public void doConnect()  {
+		
+		synchronized (this) {
+			++this.connections;
+			System.out.println("entering connect() : " + this.connections);
+		}
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		synchronized (this) {
+			--this.connections;
+			System.out.println("exiting connect(): " + this.connections);
+		}
+	}
+}
